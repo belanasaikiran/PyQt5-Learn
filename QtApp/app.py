50ffdb843a2a50f018145a6e9cacb419fa2b9947
Saikiran Belana <saikiransanju22@gmail.com>
2022-11-03 08:41:38 +0530

    enable onclick device connect

--- QtApp/app.py
+++ QtApp/app.py
@@ -1,14 +1,91 @@
+import pyfirmata
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QVBoxLayout
 
-# We use uic to load  Qt Designed UI file
-from PyQt5 import uic
+
+# ==============================================
+# Input to Arduino board for communication
+
+
+# if __name__ == '__main__':
+#     # port = input("Enter COM port Name:  \n")
+#     # comPort = port
+#     board = pyfirmata.Arduino("/dev/ttyACM0")
+#     print("Communication Successfully Initiated ")
+
+
+# ledBoard = pyfirmata.Arduino()
+
+def ConnectDevice():
+        DeviceAddress = "/dev/ttyACM0"
+
+        board = pyfirmata.Arduino(DeviceAddress)
+        if (board):
+            print("Device Connected")
+        else:
+            print("Problem Connecting to Device")
+
+
+
+
+
+# ======GET LED PIN ======
+# pin = 13
+# led = ledBoard.digital[pin]
+
+
+
+def On():
+    # led.write(1)
+    print("on")
+
+
+def Off():
+    # led.write(0)
+    print("off")
+
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("LED ON-OFF")
+
+
+        label = QLabel("Testing LED")
+
+
+
+        ConnectBoard = QPushButton("Connect Arduino")
+        ConnectBoard.clicked.connect(ConnectDevice)
+
+        turnOn_button = QPushButton("ON")
+        turnOn_button.clicked.connect(On)
+
+        turnOff_button = QPushButton("OFF")
+        turnOff_button.clicked.connect(Off)
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(ConnectBoard)
+        layout.addWidget(turnOn_button)
+        layout.addWidget(turnOff_button)
+
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
 
 
 app = QApplication(sys.argv)
 
 
 # We use uic method to load UI
-window = uic.loadUi("mainwindow.ui")
+window = MainWindow()
 window.show()
 app.exec()
--- QtApp/connectBoard.py
+++ QtApp/connectBoard.py
@@ -1,11 +1,10 @@
 import pyfirmata
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 # ==============================================
 # Input to Arduino board for communication
-
 
 if __name__ == '__main__':
     # port = input("Enter COM port Name:  \n")
@@ -19,17 +18,17 @@ pin = 13
 led = board.digital[13]
 
 
-# wait = input("Turn On/Off LED with () ? (y/n)")
-# print("You have selected", wait)
+wait = input("Turn On/Off LED with () ? (y/n)")
+print("You have selected", wait)
 
-# if wait == "y" or wait == "Y":
-#     board.digital[pin].write(1)
-#     print("LED Turned On")
-# elif wait == "n" or wait == "N":
-#     board.digital[pin].write(0)
-#     print("LED Turned Off")
-# else:
-#     print("Error!")
+if wait == "y" or wait == "Y":
+    board.digital[pin].write(1)
+    print("LED Turned On")
+elif wait == "n" or wait == "N":
+    board.digital[pin].write(0)
+    print("LED Turned Off")
+else:
+    print("Error!")
 
 
 def On():
@@ -41,43 +40,3 @@ def Off():
 
 
 
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("LED ON-OFF")
-
-
-        label = QLabel("Testing LED")
-
-        
-
-        turnOn_button = QPushButton("ON")
-        turnOn_button.clicked.connect(On)
-
-        turnOff_button = QPushButton("OFF")
-        turnOff_button.clicked.connect(Off)
-
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(turnOn_button)
-        layout.addWidget(turnOff_button)
-
-
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-
-
-
-
-app = QApplication(sys.argv)
-
-
-# We use uic method to load UI
-window = MainWindow()
-window.show()
-app.exec()
